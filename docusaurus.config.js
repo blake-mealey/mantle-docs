fbb45165668d934fc50e99188a71eba7f725f6c8
Blake Mealey <blakemealey@gmail.com>
2021-11-22 16:10:33 -0600

    fixed edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/blake-mealey/mantle-docs/edit/main/website/',
+          editUrl: 'https://github.com/blake-mealey/mantle-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
