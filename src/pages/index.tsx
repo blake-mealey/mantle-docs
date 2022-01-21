1ff7cd9237f59ce6f9630f277b03bcbcfac5f1ae
Blake Mealey <blakemealey@gmail.com>
2022-01-21 17:06:37 -0600

    improve homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -31,7 +30,75 @@ export default function Home(): JSX.Element {
     <Layout title="Features" description="Mantle features">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* TODO: extract this into data-driven components */}
+
+        <section className={styles.divider}>
+          <a href="#feature-declarative">
+            <h3>
+              <span className={styles.featureNum}>1</span> Declarative
+            </h3>
+          </a>
+          <a href="#feature-integrated">
+            <h3>
+              <span className={styles.featureNum}>2</span> Integrated
+            </h3>
+          </a>
+          <a href="#feature-automated">
+            <h3>
+              <span className={styles.featureNum}>3</span> Automated
+            </h3>
+          </a>
+        </section>
+
+        <section id="feature-declarative" className={styles.feature}>
+          <aside>
+            <img src="/img/feature_declarative.svg" />
+          </aside>
+          <div className={styles.content}>
+            <h3>
+              <span className={styles.featureNum}>1</span> Declarative
+              configuration
+            </h3>
+            <p>
+              Describe the Roblox infrastructure you want and Mantle will take
+              care of the rest.
+            </p>
+          </div>
+        </section>
+
+        <section id="feature-integrated" className={styles.feature}>
+          <aside>
+            <img src="/img/feature_integrated.svg" />
+          </aside>
+          <div className={styles.content}>
+            <h3>
+              <span className={styles.featureNum}>2</span> Integrated with the
+              Roblox OSS community
+            </h3>
+            <p>
+              Mantle integrates with the best of the Roblox OSS community.
+              Install with{' '}
+              <a href="https://github.com/roblox/foreman">Foreman</a> and build
+              with <a href="https://rojo.space">Rojo</a>.
+            </p>
+          </div>
+        </section>
+
+        <section id="feature-automated" className={styles.feature}>
+          <aside>
+            <img src="/img/feature_cd.svg" />
+          </aside>
+          <div className={styles.content}>
+            <h3>
+              <span className={styles.featureNum}>3</span> Automate with
+              continuous deployments
+            </h3>
+            <p>
+              Mantle is designed for continuous deployment environments like
+              GitHub Actions.
+            </p>
+          </div>
+        </section>
       </main>
     </Layout>
   );
