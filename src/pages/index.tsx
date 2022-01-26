59d38711ad65b8af839da5754f775244085212f5
Blake Mealey <blake.mealey@vercel.com>
2022-01-25 21:03:46 -0600

    added smart feature, improved homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,43 +27,17 @@ function HomepageHeader() {
   );
 }
 
-export default function Home(): JSX.Element {
-  return (
-    <Layout title="Features" description="Mantle features">
-      <HomepageHeader />
-      <main>
-        {/* TODO: extract this into data-driven components */}
-
-        <section className={styles.divider}>
-          <div>
-            <a href="#feature-declarative">
-              <h3>
-                <span className={styles.featureNum}>1</span>Declarative
-              </h3>
-            </a>
-            <a href="#feature-integrated">
-              <h3>
-                <span className={styles.featureNum}>2</span>Integrated
-              </h3>
-            </a>
-            <a href="#feature-automated">
-              <h3>
-                <span className={styles.featureNum}>3</span>Automated
-              </h3>
-            </a>
-          </div>
-        </section>
-
-        <div className={styles.demo}>
-          <img src="/img/mantle-demo.svg" />
-        </div>
-
-        <section id="feature-declarative" className={styles.feature}>
-          <aside>
-            <div className={styles.codeBlock}>
-              <div className={styles.codeBlockTitle}>mantle.yml</div>
-              <SyntaxHighlighter language="yaml" style={codeTheme}>
-                {`environments:
+const features = [
+  {
+    label: 'Declarative',
+    title: 'Declarative configuration',
+    description:
+      'Describe the Roblox infrastructure you want and Mantle will take care of the rest.',
+    learnMoreLink: '/docs/configuration',
+    example: {
+      title: 'mantle.yml',
+      language: 'yaml',
+      content: `environments:
   - name: dev
     targetNamePrefix: environmentName
   - name: prod
@@ -79,70 +53,75 @@ target:
         configuration:
           name: Getting Started with Mantle
           description: |-
-            Made with Mantle`}
-              </SyntaxHighlighter>
-            </div>
-          </aside>
-          <div className={styles.content}>
-            <h3>
-              <span className={styles.featureNum}>1</span>Declarative
-              configuration
-            </h3>
-            <p>
-              Describe the Roblox infrastructure you want and Mantle will take
-              care of the rest.
-            </p>
-            <div>
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/configuration"
-              >
-                Learn More
-              </Link>
-            </div>
-          </div>
-        </section>
-
-        <section id="feature-integrated" className={styles.feature}>
-          <aside>
-            <div className={styles.codeBlock}>
-              <div className={styles.codeBlockTitle}>foreman.toml</div>
-              <SyntaxHighlighter language="toml" style={codeTheme}>
-                {`[tools]
-mantle = { source = "blake-mealey/mantle", version = "0.10" }`}
-              </SyntaxHighlighter>
-            </div>
-          </aside>
-          <div className={styles.content}>
-            <h3>
-              <span className={styles.featureNum}>2</span>Integrated with the
-              Roblox OSS community
-            </h3>
-            <p>
-              Mantle integrates with the best of the Roblox OSS community.
-              Install with{' '}
-              <a href="https://github.com/roblox/foreman">Foreman</a> and build
-              with <a href="https://rojo.space">Rojo</a>.
-            </p>
-            <div>
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/installation"
-              >
-                Learn More
-              </Link>
-            </div>
-          </div>
-        </section>
-
-        <section id="feature-automated" className={styles.feature}>
-          <aside>
-            <div className={styles.codeBlock}>
-              <div className={styles.codeBlockTitle}>
-                .github/workflows/deploy.yml
-              </div>
-              <SyntaxHighlighter language="yaml" style={codeTheme}>
-                {`name: Deploy
+            Made with Mantle`,
+    },
+  },
+  {
+    label: 'Smart',
+    title: 'Smart deployments',
+    description:
+      'Mantle makes the minimum required changes to keep your deployments fast and stable.',
+    learnMoreLink: '/docs/getting-started',
+    example: {
+      title: (
+        <>
+          <span style={{ color: 'var(--ifm-color-primary)' }}>$</span> mantle
+          deploy
+        </>
+      ),
+      language: 'txt',
+      content: `Deploying resources:
+  ╷
+  │  ~ Updating: placeConfiguration_start
+  │    ╷
+  │    │  Dependencies:
+  │    │      - place:
+  │    │          assetId: 8588802093
+  │    │  Inputs:
+  │    │      placeConfiguration:
+  │    │    -   name: "[DEV] Getting Started with Mantle"
+  │    │    +   name: "[DEV] I changed the Mantle config!"
+  │    │        description: Made with Mantle
+  │    │        maxPlayerCount: 50
+  │    │        allowCopying: false
+  │    │        socialSlotType: Automatic
+  │    │        customSocialSlotsCount: ~
+  │    │
+  │    ╰─ Succeeded with outputs:
+  │           placeConfiguration
+  │
+  │
+  ╰─ Succeeded with 0 create(s), 1 update(s), 0 delete(s), 5 noop(s), 0 skip(s)`,
+    },
+  },
+  {
+    label: 'Integrated',
+    title: 'Integrated with Roblox tooling',
+    description: (
+      <>
+        Mantle integrates with the best of the Roblox OSS community. Install
+        with <a href="https://github.com/roblox/foreman">Foreman</a> and build
+        with <a href="https://rojo.space">Rojo</a>.
+      </>
+    ),
+    learnMoreLink: '/docs/installation',
+    example: {
+      title: 'foreman.toml',
+      language: 'toml',
+      content: `[tools]
+mantle = { source = "blake-mealey/mantle", version = "0.10" }`,
+    },
+  },
+  {
+    label: 'Automated',
+    title: 'Automate with continuous deployments',
+    description:
+      'Mantle is designed for continuous deployment environments like GitHub Actions.',
+    learnMoreLink: '/docs/continuous-deployment',
+    example: {
+      title: '.github/workflows/deploy.yml',
+      language: 'yaml',
+      content: `name: Deploy
 on: push
 jobs:
   build-and-deploy:
@@ -159,29 +138,74 @@ jobs:
         env:
           ROBLOSECURITY: $\{\{ secrets.ROBLOSECURITY }}
           AWS_ACCESS_KEY_ID: $\{\{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: $\{\{ secrets.AWS_SECRET_ACCESS_KEY }}`}
-              </SyntaxHighlighter>
-            </div>
-          </aside>
-          <div className={styles.content}>
-            <h3>
-              <span className={styles.featureNum}>3</span>Automate with
-              continuous deployments
-            </h3>
-            <p>
-              Mantle is designed for continuous deployment environments like
-              GitHub Actions.
-            </p>
-            <div>
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/continuous-deployment"
+          AWS_SECRET_ACCESS_KEY: $\{\{ secrets.AWS_SECRET_ACCESS_KEY }}`,
+    },
+  },
+];
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout title="Features" description="Mantle features">
+      <HomepageHeader />
+      <main>
+        {/* TODO: extract this into data-driven components */}
+
+        <section className={styles.divider}>
+          <div>
+            {features.map((feature, index) => (
+              <a
+                key={feature.label}
+                href={`#feature-${feature.label.toLowerCase()}`}
               >
-                Learn More
-              </Link>
-            </div>
+                <h3>
+                  <span className={styles.featureNum}>{index + 1}</span>
+                  {feature.label}
+                </h3>
+              </a>
+            ))}
           </div>
         </section>
+
+        <div className={styles.demo}>
+          <img src="/img/mantle-demo.svg" />
+        </div>
+
+        {features.map((feature, index) => (
+          <section
+            key={feature.label}
+            id={`feature-${feature.label.toLowerCase()}`}
+            className={styles.feature}
+          >
+            <aside>
+              <div className={styles.codeBlock}>
+                <div className={styles.codeBlockTitle}>
+                  {feature.example.title}
+                </div>
+                <SyntaxHighlighter
+                  language={feature.example.language}
+                  style={codeTheme}
+                >
+                  {feature.example.content}
+                </SyntaxHighlighter>
+              </div>
+            </aside>
+            <div className={styles.content}>
+              <h2>
+                <span className={styles.featureNum}>{index + 1}</span>
+                {feature.title}
+              </h2>
+              <p>{feature.description}</p>
+              <div>
+                <Link
+                  className="button button--secondary button--lg"
+                  to={feature.learnMoreLink}
+                >
+                  Learn More
+                </Link>
+              </div>
+            </div>
+          </section>
+        ))}
       </main>
     </Layout>
   );
