be75109c1db9f683df7220f78203c91d77262b85
Blake Mealey <blake.mealey@vercel.com>
2022-02-02 09:30:19 -0600

    added algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -167,6 +167,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        appId: 'IEEIK6DK23',
+        apiKey: '9700ffd8cc2fc0a3c1e209e243c80e26',
+        indexName: 'mantle-docs',
+      },
     }),
 };
 
